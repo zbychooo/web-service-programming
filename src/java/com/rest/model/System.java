e9b09cfae94e80e074fddde8c1428c3fa328e5fd
Zbyszko <Zbyszko@Zbyszko-laptop>
2012-12-20 19:00:24 +0100

    Model i Service classes

---
+++ src/java/com/rest/model/System.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest.model;
+
+/**
+ *
+ * @author Zbyszko
+ */
+public class System {
+    
+}
