9328940f9746544663bccd348889df45a2923b08
Zbyszko <Zbyszko@Zbyszko-laptop>
2012-12-27 15:09:25 +0100

    Change of naming convention

---
+++ src/java/com/rest/controller/ErrorsController.java
@@ -0,0 +1,11 @@
+package com.rest.controller;
+
+/**
+ *
+ * @author Zbyszko
+ */
+public class ErrorsController {
+    
+    public static String INCONSISTENT_PASSWORDS = "Sorry, given passwords are inconsistent!";
+    
+}
--- src/java/com/utilities/DBConnectorTester.java
+++ src/java/com/utilities/DBConnectorTester.java
@@ -1,11 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.utilities;
 
 /**
- *
+ * Temporal class to run and test the DBConnector class methods.
  * @author Zbyszko
  */
 public class DBConnectorTester {
