651c2488abd80b94221479480c756649841bd38d
Zbyszko <Zbyszko@Zbyszko-laptop>
2012-12-27 18:56:35 +0100

    Register user works! Added encrypting password, first tries of login

--- src/java/com/rest/controller/ErrorsController.java
+++ src/java/com/rest/controller/ErrorsController.java
@@ -8,7 +8,9 @@ public class ErrorsController {
     
     public static String INCONSISTENT_PASSWORDS = "Sorry, given passwords are inconsistent.";
     public static String EMPTY_FIELDS = "Some fields are empty.";
-    public static String IMPROPER_STRING_LENGTH_OR_FORMAT = "The format of length of the string is improper.";
-    public static String ONLY_LETTERS_ALLOWED = "Only letters are allowed.";
+    public static String IMPROPER_STRING_LENGTH_OR_FORMAT = "The format of length of the string is improper: ";
+    public static String ONLY_LETTERS_ALLOWED = "Only letters are allowed: ";
+    public static String LOGIN_TAKEN = "Given login is already registered.";
+    public static String INVALID_CREDENTIALS = "Invalid credentials";
     
 }
