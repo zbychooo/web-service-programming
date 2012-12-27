56bc9f05ded141d163c74fd5f5b465f115567391
Zbyszko <Zbyszko@Zbyszko-laptop>
2012-12-27 14:48:31 +0100

    DB connection test

--- src/java/com/rest/service/UserService.java
+++ src/java/com/rest/service/UserService.java
@@ -4,10 +4,27 @@
  */
 package com.rest.service;
 
+import com.rest.controller.UserController;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author Zbyszko
  */
 public class UserService {
-    
+
+    @POST
+    @Path("/register")
+    public Response addUser(@FormParam("login") String login, 
+                            @FormParam("password") String password,
+                            @FormParam("confirm_password") String confirmPassword,
+                            @FormParam("username") String username) {
+        
+        int returnCode = UserController.checkUser(login, password, confirmPassword, username);
+        
+        return null;
+    }
 }
