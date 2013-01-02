bb2f261cfa491d82c6c743dc2183da426c3aa707
Atuan <Atuan@Lapek>
2013-01-02 14:04:01 +0100

    Remove 'my account' menutab

---
+++ src/java/com/rest/service/UserService.java
@@ -0,0 +1,60 @@
+package com.rest.service;
+
+import com.rest.controller.UsersController;
+import com.sun.jersey.spi.resource.Singleton;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ * User Service class.
+ *
+ * @author Zbyszko
+ */
+@Path("/userService")
+@Singleton
+public class UserService {
+
+    UsersController usersController = new UsersController();
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response addUser(@FormParam("login") String login,
+            @FormParam("password") String password,
+            @FormParam("confirm_password") String confirmPassword,
+            @FormParam("username") String username) {
+
+        String returnMessage = usersController.checkUser(login, password, confirmPassword, username);
+
+        if (returnMessage.equals("OK")) {
+            usersController.add(login, password, username);
+        }
+        return Response.ok().entity("System return message: " + returnMessage).build();
+    }
+
+    @GET
+    @Path("/login")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response loginUser(@Context SecurityContext sec) {
+        String out = "Hello " + sec.getUserPrincipal().getName()
+                + ", is secure? " + sec.isSecure();
+        System.out.println(out);
+        
+        return Response.status(Response.Status.OK).entity("OK").build();
+    }
+    
+    @GET
+    @Path("/getUserLogin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getUserLogin(@Context SecurityContext sec){
+        return Response.ok().entity(sec.getUserPrincipal().getName()).build();
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head> 
+        <meta http-equiv="refresh" content="0; URL='rest/home/0'" />
+    </head>
+    <body>
+<!--                    <a href="rest/systemService/getAvailableStorageSize">get total space size</a><br/>
+                    <a href="rest/userService/myfiles">my files </a> |
+                    <form method="POST" action="rest/systemService/createFolder">
+                        <input type="text" name="foldername" /> <input type="submit" value="New folder" />
+                    </form>-->
+                    
+                    
+    </body>
+</html>
