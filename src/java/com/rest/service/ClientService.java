5ad71b9eeb7950c7befaca80045dbce65de42a49
Zbyszko <Zbyszko@Zbyszko-laptop>
2013-01-03 17:08:46 +0100

    szukajka plik?w po nazwach pliku i tagach

---
+++ src/java/com/rest/service/ClientService.java
@@ -0,0 +1,106 @@
+package com.rest.service;
+
+import com.rest.client.SystemClient;
+import com.rest.controller.UsersController;
+import com.rest.model.Folder;
+import com.rest.model.User;
+import com.sun.jersey.spi.resource.Singleton;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ *
+ * @author Atuan
+ */
+@Singleton
+@Path("/")
+public class ClientService {
+    
+    private SystemClient systemClient;
+    
+    @GET
+    @Path("/home/{cfi}")
+    @Produces("text/html")
+    public Response index1(@PathParam("cfi") String currentFolderIndex, @Context HttpServletRequest request, @Context SecurityContext sec) {
+        //initialize model
+        Map<String, Object> map = new HashMap<String, Object>();
+        //add stuff necessary for the page to display correctly
+        SystemService ss = new SystemService();
+//        if(sec.getUserPrincipal() == null){
+//            return Response.ok(new Viewable("/login")).build();
+//        }
+        Response temp = ss.getRemainingStorageSize(sec);
+        System.out.println("actually inside the method "+(String)temp.getEntity());
+        map.put("remainingSpace", (String)temp.getEntity());
+        try{
+            UsersController uc = new UsersController();
+            User currentUser = (User)uc.getUsers().get(sec.getUserPrincipal().getName());
+            request.getSession().setAttribute("user", currentUser);
+            request.getSession().setAttribute("remainingSpace", (String)temp.getEntity());
+            System.out.println("request is working :| ");
+            systemClient = new SystemClient(currentUser);
+            List<Folder> folders = systemClient.getFolderList();
+            if(folders == null){
+                folders = new ArrayList<>();
+            }
+            request.getSession().setAttribute("folders", folders);
+            System.out.println("before cfi casting");
+            request.getSession().setAttribute("currentFolderIndex", Long.valueOf(currentFolderIndex));
+            System.out.println("request2");
+        } catch(Exception e){
+            System.out.println("request ain't workin'.");
+        }
+        //return a page
+        try{
+            return Response.temporaryRedirect(new URI("../home.jsp")).build();
+        } catch(Exception e){
+            return Response.ok(":(").build();
+        }
+    }
+    
+    @GET
+    @Path("/myfolders/{mode}")
+    @Produces("text/html")
+    public Response index(@PathParam("mode") String mode, @Context HttpServletRequest request, @Context SecurityContext sec) {
+        SystemService ss = new SystemService();
+        Response temp = ss.getRemainingStorageSize(sec);
+        try{
+            UsersController uc = new UsersController();
+            User currentUser = (User)uc.getUsers().get(sec.getUserPrincipal().getName());
+            request.getSession().setAttribute("user", currentUser);
+            request.getSession().setAttribute("remainingSpace", (String)temp.getEntity());
+            systemClient = new SystemClient(currentUser);
+            List<Folder> folders = new ArrayList<>();
+            for(Folder f : systemClient.getFolderList()){
+                if(mode.equals("all")){
+                    folders.add(f);
+                } else if(mode.equals("shared")){
+                    if(!f.getShared().isEmpty()){
+                        folders.add(f);
+                    }
+                }
+            }
+            request.getSession().setAttribute("folders", folders);
+            System.out.println("after folders");
+        } catch(Exception e){
+            System.out.println("request ain't workin'.");
+        }
+        //return a page
+        try{
+            return Response.temporaryRedirect(new URI("../myfolders.jsp")).build();
+        } catch(Exception e){
+            return Response.ok(":(").build();
+        }
+    }
+}
---
+++ src/java/com/rest/service/SystemService.java
@@ -0,0 +1,250 @@
+package com.rest.service;
+
+import com.rest.controller.ErrorsController;
+import com.rest.controller.SystemController;
+import com.rest.model.Folder;
+import com.rest.model.User;
+import com.rest.model.UserFile;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+import com.sun.jersey.spi.resource.Singleton;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+
+/**
+ * System service class.
+ *
+ * @author Zbyszko
+ */
+@Path("/systemService")
+@Singleton
+public class SystemService {
+
+    SystemController systemController = new SystemController();
+
+    @GET
+    @Path("/createSpace")
+    public Response createSpace(@Context SecurityContext sec) {
+
+        boolean isCreated = systemController.createSpace(sec.getUserPrincipal().getName());
+
+        if (isCreated) {
+            return Response.ok().entity("isCreated: " + isCreated).build();
+        }
+        return Response.ok().entity("Error: " + ErrorsController.SPACE_ALREADY_CREATED).build();
+    }
+
+    @POST
+    @Path("/createFolder")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createFolder(@FormParam("folderName") String folderName, @Context SecurityContext sec) {
+
+        String login = sec.getUserPrincipal().getName();
+
+        boolean isCreated = systemController.createFolder(
+                folderName, login);
+        
+        String path = login +  "//" + folderName;
+        if (isCreated) {
+            Long folderId = systemController.addFolderInfoToDB(folderName, path);
+            systemController.joinFolderAndUser(folderId, login, 1);
+            
+            return Response.ok().entity("isCreated: " + isCreated).build();
+        }
+        return Response.ok().entity("Error: " + ErrorsController.FOLDER_ALREADY_EXISTS).build();
+    }
+
+    @POST
+    @Path("/uploadFile")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@FormDataParam("file") InputStream in, 
+        @FormDataParam("file") FormDataContentDisposition info, 
+        @FormDataParam("tag") String tag, @FormDataParam("path") String path,
+        @Context SecurityContext sec) {
+
+        String userlogin = sec.getUserPrincipal().getName();
+        path = userlogin + "//" + path; 
+        //TODO: sprawdzic czy nie folder usera nie przekracza max. pojemności!!!1
+        
+        if(userlogin==null){
+            return Response.serverError().build();
+        }
+        tag = tag.toUpperCase();            
+        Long fileSize = systemController.uploadFile(in, info.getFileName(), path, userlogin);
+        
+        if (fileSize== -1) {
+            return Response.ok().entity("Error: " + ErrorsController.UPLOAD_ERROR).build();
+        }
+        
+        // zapisanie informacji o pliku w bazie danych
+        Long fileId = systemController.addFileInfoToDB(info.getFileName(), fileSize, tag, path);
+        // zapisanie informacji o właścicielu pliku
+        systemController.joinFileAndFolder(fileId, path);
+
+        return Response.ok().entity("File is up.").build();
+    }
+
+    /**
+     * 
+     * @param filePath wysyłamy wg formatu {folderGłówny}//{nazwaPodfolderu}
+     * @param shareLogin
+     * @param sec
+     * @return 
+     */
+    @POST
+    @Path("/shareFolder")
+    public Response shareFile(@FormParam("filePath") String filePath, @FormParam("shareLogin") String shareLogin, @Context SecurityContext sec) {
+        
+        long folderId = systemController.getFolderId(filePath);
+        if(folderId!=0){
+            boolean isOwner = systemController.isFolderOwner(folderId, sec.getUserPrincipal().getName());  
+            if(isOwner){
+                systemController.joinFolderAndUser(folderId, shareLogin, 0);
+            }
+            else {
+                return Response.ok().entity("user is not owner of this file").build();
+            }
+        } else {
+            return Response.ok().entity("cannot get folder id").build();
+        }    
+        return Response.ok().entity("ok, folder shared").build();
+    }
+    
+    @POST
+    @Path("/unshareFolder")
+     public Response unshareFile(@FormParam("filePath") String filePath, @Context SecurityContext sec){
+        
+        long folderId = systemController.getFolderId(filePath);
+        if(folderId!=0){
+            boolean isOwner = systemController.isFolderOwner(folderId, sec.getUserPrincipal().getName());    
+            if(isOwner){
+                systemController.disjoinFolderAndNoOwnerUsers(folderId, sec.getUserPrincipal().getName());
+            }
+            else {
+                return Response.ok().entity("user is not owner of this file").build();
+            }
+        } else {
+            return Response.ok().entity("cannot get folder id").build();
+        }    
+        return Response.ok().entity("ok, folder UNshared").build();
+    }   
+    
+    @POST
+    @Path("/search")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response search(@FormParam("searchPhrase") String phrase){
+        
+        ArrayList<UserFile> results = systemController.search(phrase);
+        return Response.ok().entity("Results: \n" + results.toString()).build();
+    } 
+    
+    @GET
+    @Path("/deleteFolder/{folderPath}")
+    public Response deleteFolder(@PathParam("folderPath") String path, @Context SecurityContext sec){
+        path = sec.getUserPrincipal().getName() + "//" + path;
+        
+        
+        boolean isDeleted = systemController.deleteFolderFromDB(path, sec.getUserPrincipal().getName());
+        if(!isDeleted){
+            return Response.ok().entity(ErrorsController.DELETION_ERROR).build();
+        }
+        isDeleted = systemController.deleteFolder(path);
+        
+        if(!isDeleted){
+            return Response.ok().entity(ErrorsController.DELETION_ERROR).build();
+        }
+        
+        return Response.ok().entity("ok").build();
+    }
+    
+    @GET
+    @Path("/deleteFile/{folderPath}/{fileName}")
+    public Response deleteFile(@PathParam("folderPath") String path, @PathParam("fileName") String fileName, @Context SecurityContext sec){
+
+        String login = sec.getUserPrincipal().getName();
+        path = login + "//" + path; 
+        
+        boolean isDeleted = systemController.deleteFileFromDB(path, fileName, login);
+        System.out.println("check: " + isDeleted);
+        if(!isDeleted) {
+            return Response.ok().entity(ErrorsController.DELETION_ERROR).build();
+        }
+        isDeleted = systemController.deleteFile(path, fileName);
+        
+        if(!isDeleted){
+            return Response.ok().entity(ErrorsController.DELETION_ERROR).build();
+        }
+        
+        return Response.ok().entity("ok").build();
+    }
+    
+    @GET
+    @Path("/getRemainingStorageSize")
+    @Produces("text/plain")
+    public Response getRemainingStorageSize(@Context SecurityContext sec) {
+        
+        try{
+            long folderSize = systemController.getFolderSize(sec.getUserPrincipal().getName());
+            long availableSpace = SystemController.MAX_STORAGE - folderSize;
+            Double availableSpaceD = availableSpace / 1000.0;
+            
+            return Response.ok().entity(Double.toString(availableSpaceD)).build();
+        } catch(Exception e){
+            return Response.ok().entity("0").build();
+        }
+    }
+    
+    @GET
+    @Path("/myfolders")
+    @Produces(MediaType.APPLICATION_XML) 
+//    public Response getCurrentUserFolders(@Context SecurityContext sec){
+    public List<Folder> getCurrentUserFolders(@Context SecurityContext sec){
+        List<Folder> folders = new ArrayList<>(); 
+        
+        List<UserFile> files2 = new ArrayList<>();
+//        List<String> tags2 = new ArrayList<>();
+//        tags2.add("TV-Series");
+//        tags2.add("Favorites");
+        Date date = new java.util.Date();
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String currentDate = dateFormat.format(date);
+        
+        files2.add(new UserFile(Long.valueOf(1), "S01E01.avi", Long.valueOf(351310000),currentDate,"TV-Series",""));        
+        files2.add(new UserFile(Long.valueOf(2), "S01E02.avi", Long.valueOf(350610000),currentDate,"TV-Series",""));        
+        files2.add(new UserFile(Long.valueOf(3), "S01E03.avi", Long.valueOf(351610070),currentDate,"TV-Series",""));
+        folders.add(new Folder(Long.valueOf(177),
+                new User(Long.valueOf(10),"seba1","","Seba","user"),"The Vampire Diaries",new ArrayList<User>(),files2));
+        
+        List<UserFile> files1 = new ArrayList<>();
+        files1.add(new UserFile(Long.valueOf(1), "01 3 Hours.mp3",Long.valueOf(7310000),currentDate,"Music",""));
+        
+        files1.add(new UserFile(Long.valueOf(2), "01 Common exchange.mp3",Long.valueOf(6610000),currentDate,"Music",""));
+        folders.add(new Folder(Long.valueOf(173),
+                new User(Long.valueOf(9),"seba","seba","Seba","user"),"folderek",new ArrayList<User>(),files1));
+        
+        folders.add(new Folder(Long.valueOf(174),new User(),"kolejny glupi rok",new ArrayList<User>()));
+        
+        folders.addAll(systemController.getUserFolders(new User(Long.valueOf(10),"grimm","grimm","Grimm","user")));
+        
+//        ListResponse listResponse = new ListResponse(folders);
+//        GenericEntity entity = new GenericEntity<List<Folder>>(folders) {};
+        System.out.println("MY FOLDERS "+folders.size());
+        return folders;
+//        return Response.ok(folders, MediaType.APPLICATION_XML).build();
+//        return Response.ok(folders.get(0), MediaType.APPLICATION_XML).build();
+    }
+    
+}
