6d458eca25475aac1f901e45b88ff7a50b8d0e7a
Atuan <Atuan@Lapek>
2013-01-03 20:21:04 +0100

    Modified method getUserFolders, includes also folder files.

---
+++ src/java/com/utilities/DBConnector.java
@@ -0,0 +1,74 @@
+package com.utilities;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * External database credentials and connection methods
+ * @author Zbyszko
+ */
+public class DBConnector {
+
+    //DO NOT CREATE SETTERS AND/OR GETTERS FOR THESE FIELDS!
+    private String driverName = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://www.a2l.pl:3306/a2l_restcloud";
+    private String uid = "a2l_restcloud";
+    private String pwd = "restcloud!!2012";
+    //---------------------------------------------------------
+    private Connection connection;
+
+    /**
+     * Checks if database connection is possible
+     * @return 
+     */
+    public boolean isConnection() {
+        
+        try {
+            Class.forName(driverName);
+            connection = (Connection) DriverManager.getConnection(url, uid, pwd);
+
+           PreparedStatement statement = connection.prepareStatement("select * from tuser");
+           ResultSet rs = statement.executeQuery();
+           while(rs.next()) {
+               System.out.print(rs.getString("id"));
+               System.out.println(" " + rs.getString("tname"));
+           }
+           rs.close();
+           statement.close();   
+           connection.close();        
+        } catch (ClassNotFoundException exc) {  // brak klasy sterownika
+            System.out.println("No class driver");
+            System.out.println(exc);
+            return false;
+        } catch (SQLException exc) {  // nieudane połączenie
+            System.out.println("Connection failed, url: " + url);
+            System.out.println(exc);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Connects to database.
+     * @return Connection Object.
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(driverName);
+        connection = (Connection) DriverManager.getConnection(url, uid, pwd);
+        return connection;
+    }
+
+    /**
+     * Closes the database connection.
+     * @throws SQLException 
+     */
+    public void closeConnection() throws SQLException {
+        connection.close();
+    }
+}
---
+++ web/upload.jsp
@@ -0,0 +1,49 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP !TEST! Page</title>
+    </head>
+    <body>
+        <h1>Upload:</h1>
+        <form action="rest/systemService/uploadFile" method="POST" enctype="multipart/form-data">
+            file: <input type="file" name="file" /><br />
+            tag: <input type="text" name="tag" size="50" value="" /><br />
+            dest. path: <select name="path">
+                <option>baluty</option>
+                <option>widzew</option>
+                <option>polesie</option>
+            </select><br />
+            <input type="submit" value="upload" />
+        </form>
+        
+        <hr /><h1>CreateFolder:</h1>
+        <form action="rest/systemService/createFolder" method="POST">
+            folder name: <input type="text" name="folderName" size="50" value="" /><br />
+            <input type="submit" value="create" />
+        </form>
+        
+        <hr /><h1>Menu:</h1>
+        <ul>
+            <li><a href="rest/systemService/deleteFile/widzew/zaal.txt">delete zaal.txt</a></li>
+            <li><a href="rest/systemService/deleteFolder/baluty">delete folder baluty</a></li>
+        </ul>
+        
+        
+        <hr /><h1>ShareFolder:</h1>
+        <form action="rest/systemService/shareFolder" method="POST">
+            file path [format: {folderGłówny}//{nazwaPodfolderu}]: <input type="text" name="filePath" size="50" value="" /><br />
+            share login: <input type="text" name="shareLogin" size="50" value="" /><br />
+            <input type="submit" value="share" />
+        </form>
+        
+        <h1><u>UN</u>ShareFolder:</h1>
+        <form action="rest/systemService/unshareFolder" method="POST">
+            file path [format: {folderGłówny}//{nazwaPodfolderu}]: <input type="text" name="filePath" size="50" value="" /><br />
+            <input type="submit" value="UNshare" />
+        </form>
+        
+        <hr />
+    </body>
+</html>
